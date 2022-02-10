5dbb1d345a87c5efe0bfa53d7f237994e42d9b09
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2022-02-10 21:01:00 +0000

    1 - Immagini casuali ma non solo due volte ciascuna_Click funzionanti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-card-game';
-  cards:{ cardID: number , status:string , path:string }[]=[ 
-    { cardID:0 , status:'default' , path: './assets/default.png' },
-    { cardID:1 , status:'default' , path: './assets/default.png' },
-  ] 
- 
+  cards:{ cardID: number , status:string , path:string, nImm:number }[] = [];
+  posizioni:{ n:number, volte:number}[] = [
+    {n:0, volte:0}, {n:1, volte:0}, {n:2, volte:0}, {n:3, volte:0},{n:4, volte:0},
+    {n:5, volte:1}, {n:6, volte:0}, {n:7, volte:1}, {n:8, volte:0}, {n:9, volte:0}
+  ];
+
+  ngOnInit(): void {
+    for (var i = 0; i < 20; i++) {
+      var x = 0;
+      //do {
+        x = Math.floor(Math.random() * 10); //da 0 a 9
+      //} while (this.posizioni[x].volte == 2);
+      this.posizioni[x].volte++;
+      this.cards.push({
+        cardID: i, status:'default', path:'./assets/images/default.png', nImm: x
+      });
+    }
+    console.log(this.posizioni);
+  }
 
   fromFiglioEvntHandlr( evntData: { cardID: number , status:string , path:string } ){
-
-    console.log("Sono il padre: dal figlio numero " + evntData.cardID+  " ho ricevuto status="+evntData.status )
+    console.log("Sono il padre: dal figlio numero " + evntData.cardID + " ho ricevuto status="+evntData.status)
     if (this.cards[evntData.cardID].status=='default') {
       this.cards[evntData.cardID].status='flipped'
-      this.cards[evntData.cardID].path='./assets/3cuori.png'
+      this.cards[evntData.cardID].path='./assets/images/' + this.cards[evntData.cardID].nImm + '.jpg'
     } else {
       this.cards[evntData.cardID].status='default'
-      this.cards[evntData.cardID].path='./assets/default.png'
+      this.cards[evntData.cardID].path='./assets/images/default.png'
     }
-    
-    console.log(evntData)
   }
 }
