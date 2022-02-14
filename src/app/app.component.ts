c8e30a7b8234628f97d1fb0ce213219199636936
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2022-02-14 21:32:29 +0000

    non so cosa scrivere :(

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'angular-card-game';
-  cards:{ cardID: number , status:string , path:string, nImm:number }[] = [];
-  posizioni:{ n:number, volte:number}[] = [
-    {n:0, volte:0}, {n:1, volte:0}, {n:2, volte:0}, {n:3, volte:0},{n:4, volte:0},
-    {n:5, volte:1}, {n:6, volte:0}, {n:7, volte:1}, {n:8, volte:0}, {n:9, volte:0}
-  ];
+  cards:{ cardID: number , status:string , path:string, nImm:number, check:boolean }[];
+  posizioni:{ n:number, volte:number}[];
+  carta1:number;
+  carta2:number;
+  punteggio:number;
+  click:boolean;
+  mostra:string;
 
   ngOnInit(): void {
+    this.cards = this.posizioni = [];
+    this.carta1 = this.carta2 = -1;
+    this.punteggio = 0;
+    this.click = true;
+    this.mostra = "hidden";
+    for (var i = 0; i < 10; i++) this.posizioni.push({n:i, volte:0});
     for (var i = 0; i < 20; i++) {
       var x = 0;
-      //do {
+      do {
         x = Math.floor(Math.random() * 10); //da 0 a 9
-      //} while (this.posizioni[x].volte == 2);
+      } while (this.posizioni[x].volte == 2);
       this.posizioni[x].volte++;
       this.cards.push({
-        cardID: i, status:'default', path:'./assets/images/default.png', nImm: x
+        cardID: i, status:'default', path:'./assets/images/default.png', nImm: x, check:false
       });
     }
-    console.log(this.posizioni);
   }
 
-  fromFiglioEvntHandlr( evntData: { cardID: number , status:string , path:string } ){
-    console.log("Sono il padre: dal figlio numero " + evntData.cardID + " ho ricevuto status="+evntData.status)
-    if (this.cards[evntData.cardID].status=='default') {
-      this.cards[evntData.cardID].status='flipped'
-      this.cards[evntData.cardID].path='./assets/images/' + this.cards[evntData.cardID].nImm + '.jpg'
-    } else {
-      this.cards[evntData.cardID].status='default'
-      this.cards[evntData.cardID].path='./assets/images/default.png'
+  fromFiglioEvntHandlr( evntData: { cardID: number , status:string , path:string, check:boolean } ){
+    if (!evntData.check && this.click) {
+      if (this.cards[evntData.cardID].status=='default') {
+        this.cards[evntData.cardID].status='flipped'
+        this.cards[evntData.cardID].path='./assets/images/' + this.cards[evntData.cardID].nImm + '.jpg'
+      } else {
+        this.cards[evntData.cardID].status='default'
+        this.cards[evntData.cardID].path='./assets/images/default.png'
+      }
+
+      if (this.carta1 == -1) {
+        this.carta1 = evntData.cardID;
+      } else if (this.carta1 != -1 && this.carta2 == -1) {
+        this.carta2 = evntData.cardID;
+        if (this.cards[this.carta1].nImm == this.cards[this.carta2].nImm) {
+          this.cards[this.carta1].check = true;
+          this.cards[this.carta2].check = true;
+          this.carta1 = this.carta2 = -1;
+          this.punteggio++;
+          this.controllo();
+        } else {
+          this.click = false;
+          setTimeout(() => {
+            this.cards[this.carta1].status='default';
+            this.cards[this.carta1].path='./assets/images/default.png';
+            this.cards[this.carta2].status='default';
+            this.cards[this.carta2].path='./assets/images/default.png';
+            this.click = true;
+            this.carta1 = this.carta2 = -1;
+          }, 1000);
+        }
+      }
+    }
+  }
+
+  controllo() {
+    var vinto = true;
+    this.cards.forEach(x => {
+      if (!x.check) vinto = false;
+    });
+    if (vinto) {
+      alert("Hai vinto!!!");
+      //resettare e randomizzare di nuovo
     }
   }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  @Input()  cardData:{ cardID: number , status:string , path:string }
-  @Output() fromFiglioEvnt = new EventEmitter<{ cardID: number , status:string , path:string }>();
+  @Input()  cardData:{ cardID: number , status:string , path:string, check:boolean }
+  @Output() fromFiglioEvnt = new EventEmitter<{ cardID: number , status:string , path:string, check:boolean }>();
 
   constructor() { }
 
